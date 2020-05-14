b0058b263fcd517ce3494384536e760915e0181f
MirMiao <1251267855@qq.com>
2020-05-14 17:32:27 +0800

    设置的点击事件

---
+++ app/src/main/java/com/itheima/player/ui/activity/SettingActivity.kt
@@ -0,0 +1,15 @@
+package com.itheima.player.ui.activity
+
+import com.itheima.player.R
+import com.itheima.player.base.BaseActivity
+
+/**
+ * 时间 :2020/5/14  17:23
+ * 作者 :苗恒
+ * 功能 :
+ */
+class SettingActivity:BaseActivity() {
+    override fun getLayoutId(): Int {
+        return R.layout.activity_setting
+    }
+}
--- app/src/main/java/com/itheima/player/util/ToolBarManager.kt
+++ app/src/main/java/com/itheima/player/util/ToolBarManager.kt
@@ -1,9 +1,15 @@
 package com.itheima.player.util
 
+import android.content.Intent
 import android.os.Build
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.Toolbar
 import com.itheima.player.R
+import com.itheima.player.ui.activity.SettingActivity
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.toast
 
 /**
  * 时间 :2020/5/14  11:02
@@ -19,6 +25,22 @@ interface ToolBarManager {
     fun initMainToolBar(){
         toolbar.setTitle("黑马影音")
         toolbar.inflateMenu(R.menu.main)
+        //kotlin和java的特性
+        //如果java接口中只有一个未实现的方法 可以省略接口对象，直接用{}表示未实现的方法
+        toolbar.setOnMenuItemClickListener{
+            toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+              true
+        }
+       /* toolbar.setOnMenuItemClickListener(object:Toolbar.OnMenuItemClickListener{
+            override fun onMenuItemClick(item: MenuItem?): Boolean {
+                when(item?.itemId){
+                    R.id.setting->{
+                         toolbar.context.startActivity(Intent(toolbar.context,SettingActivity::class.java))
+                    }
+                }
+                return true
+            }
+        })*/
 
     }
 }
